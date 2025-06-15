622c1209ee0b6fd08621e64287d81878eab5f184
shirz <shir.shlomoarzi@gmail.com>
2025-06-15 17:35:39 +0300

    first commit - base playwright infrastructure

---
+++ src/setup/hooks.ts
@@ -0,0 +1,14 @@
+import {navigateToPage} from "../support/navigation-behavior"
+import {globalConfig} from "../index"
+import {test as baseTest} from '@playwright/test'
+
+
+export const test = baseTest.extend({})
+
+test.beforeEach(async ({ page }) => {
+    await navigateToPage(page, "home", globalConfig)
+})
+
+test.afterEach(async ({ page }) => {
+    await page.close()
+})
