139213179b5b7ffe93ac7a8d6d19fe550293866d
modi-ennekavi <modiennekavi@gmail.com>
2025-06-23 16:13:10 +0300

    Task 3

--- src/features/1_createPage.spec.ts
+++ src/features/1_createPage.spec.ts
@@ -1,8 +1,24 @@
-import { expect } from "playwright/test"
+import { expect, Page } from "playwright/test"
 import { test } from "../setup/hooks"
-import { fillContactInfo } from "../steps/ceration-form-steps";
+import { Contact, fillContactInfo, getErrorMessage, omitFieldFromContact } from "../steps/ceration-form-steps";
 import { getContactNames } from "../steps/contact-steps";
 
+const contact: Contact = {
+    name: "Genghis Khan",
+    gender: "Male",
+    phone: "(577) 385-0576",
+    street: "Ap #826-8849 Vulputate Street",
+    city: "Temüjin"
+}
+
+const checkMissingField = async (page: Page, fieldToOmit: keyof Contact): Promise<void> => {
+    await fillContactInfo(page, omitFieldFromContact(contact, fieldToOmit))
+
+    await page.getByRole("button", { name: "Save" }).click();
+
+    const errorMessage: string = await getErrorMessage(page)
+    expect(errorMessage).toBe(`Error: The "${fieldToOmit}" field can't be empty.`)
+}
 
 /*
     Here you write all the scenarios of the createPage.
@@ -13,13 +29,30 @@ import { getContactNames } from "../steps/contact-steps";
     So, when writing a scenario here, write the small steps in a scenario function
     and write the scenario function in here
 */
-test.only("Create new contact", async ({ page }): Promise<void> => {
+test("Create new contact", async ({ page }): Promise<void> => {
     await page.getByRole("button", { name: "Create" }).click();
 
-    await fillContactInfo(page, "Genghis Khan", "Male", "(577) 385-0576",
-        "Ap #826-8849 Vulputate Street", "Temüjin")
-
+    await fillContactInfo(page, contact)
     await page.getByRole("button", { name: "Save" }).click();
-
     expect(await getContactNames(page)).toContain("Genghis Khan")
+})
+
+test("Create new contact, missing name", async ({ page }): Promise<void> => {
+    await page.getByRole("button", { name: "Create" }).click();
+    await checkMissingField(page, "name")
+})
+
+test("Create new contact, missing phone", async ({ page }): Promise<void> => {
+    await page.getByRole("button", { name: "Create" }).click();
+    await checkMissingField(page, "phone")
+})
+
+test("Create new contact, missing street", async ({ page }): Promise<void> => {
+    await page.getByRole("button", { name: "Create" }).click();
+    await checkMissingField(page, "street")
+})
+
+test("Create new contact, missing city", async ({ page }): Promise<void> => {
+    await page.getByRole("button", { name: "Create" }).click();
+    await checkMissingField(page, "city")
 })
--- src/steps/ceration-form-steps.ts
+++ src/steps/ceration-form-steps.ts
@@ -2,22 +2,44 @@ import { Locator, Page } from "playwright/test"
 import { getElement } from "../support/elements-helper"
 import { globalConfig } from "../index"
 
+export type Contact = {
+    name?: string,
+    gender?: string,
+    phone?: string,
+    street?: string,
+    city?: string
+}
 
-export const fillTextInput = async (page: Page, fieldName: string, text: string) => {
+export const fillTextInput = async (page: Page, fieldName: string, text: string): Promise<void> => {
     const nameField: Locator = await getElement(page, fieldName, globalConfig)
     await nameField.fill(text)
 }
 
-export const fillSelectInput = async (page: Page, fieldName: string, text: string) => {
+export const fillSelectInput = async (page: Page, fieldName: string, text: string): Promise<void> => {
     const nameField: Locator = await getElement(page, fieldName, globalConfig)
     await nameField.selectOption(text)
 }
 
-export const fillContactInfo = async (page: Page, name: string, gender: string,
-    phone: string, street: string, city: string) => {
-    await fillTextInput(page, "name field", name)
-    await fillSelectInput(page, 'gender field', gender)
-    await fillTextInput(page, 'phone field', phone)
-    await fillTextInput(page, 'street field', street)
-    await fillTextInput(page, 'city field', city)
+export const fillContactInfo = async (page: Page, contact: Contact): Promise<void> => {
+    if (contact.name) await fillTextInput(page, "name field", contact.name)
+    if (contact.gender) await fillSelectInput(page, 'gender field', contact.gender)
+    if (contact.phone) await fillTextInput(page, 'phone field', contact.phone)
+    if (contact.street) await fillTextInput(page, 'street field', contact.street)
+    if (contact.city) await fillTextInput(page, 'city field', contact.city)
+}
+
+export const getErrorMessage = async (page: Page): Promise<string> => {
+    const errorMessage: Locator = await getElement(page, "error message", globalConfig)
+    return await errorMessage.innerText()
+}
+
+export const omitFieldFromContact = (contact: Partial<Contact>, keyToOmit: keyof Contact): Partial<Contact> => {
+    let partial: Partial<Contact> = {}
+
+    for (const key in contact) {
+        if (key === keyToOmit) continue;
+        partial[key] = contact[key]
+    }
+
+    return partial;
 }
