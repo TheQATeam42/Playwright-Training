5672e0e481579d16d641aa6c4a1fd24ea677c911
Shvartz-Roy <therealroy26@gmail.com>
2026-03-03 20:59:00 +0200

    thired test- negative contact creation

--- srcReactApp/pages/createContact.page.ts
+++ srcReactApp/pages/createContact.page.ts
@@ -1,6 +1,13 @@
 import { Locator, Page } from "@playwright/test";
 import BasePage from "../../sharedFiles/pages/basePage.page";
 
+export enum ContactData {
+  name = "name",
+  phone = "phone",
+  street = "street",
+  city = "city",
+}
+
 /**
  * Represents the create page of the application.
  * This class provides methods and properties to interact with the create page elements.
@@ -12,6 +19,7 @@ export default class CreateContact extends BasePage {
   public readonly page: Page;
   public readonly createContactHeader: Locator;
   public readonly saveContact: Locator;
+  public readonly errorMessage: Locator;
   // recieve a form field name and get the locator for that field
   public formField(fieldName: string): Locator {
     return this.page.locator(`input[data-id="${fieldName}"]`);
@@ -24,5 +32,23 @@ export default class CreateContact extends BasePage {
       'h1[data-id="create-contact-header"]'
     );
     this.saveContact = this.page.locator('button[data-id="save-button"]');
+    this.errorMessage = this.page.locator('div[data-id="error-message"]');
+  }
+
+  async fillFormExceptField(fieldToExclude: ContactData): Promise<void> {
+    const fields = Object.values(ContactData);
+    for (const field of fields) {
+      if (field === fieldToExclude) {
+        continue;
+      }
+      await this.formField(field).fill("123456789");
+    }
+  }
+
+  async clearForm(): Promise<void> {
+    const fields = Object.values(ContactData);
+    for (const field of fields) {
+      await this.formField(field).fill("");
+    }
   }
 }
--- srcReactApp/tests/contactsListCreate.spec.ts
+++ srcReactApp/tests/contactsListCreate.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from "@playwright/test";
 import Contacts from "../pages/contacts.page";
-import CreateContact from "../pages/createContact.page";
+import CreateContact, { ContactData } from "../pages/createContact.page";
 import reactAppTest from "./setup/testLevelHooks.setup";
 
 /**
@@ -55,5 +55,33 @@ contactsListCreateTest(
 
     // step 10: make sure the contact is not visible in the list after refresh
     await expect(contacts.contactCard(contactName)).not.toBeVisible();
+  }
+);
+
+const contactsListNegativeCreateTest = reactAppTest.extend({});
+contactsListNegativeCreateTest(
+  "negative contact creation and verify behavior",
+  async ({ page }) => {
+    // step 1: navigate to the contacts list page and verify the create button is visible
+    const contacts = new Contacts(page);
+    await expect(contacts.createButton).toBeVisible();
+
+    // step 2: click the create button to add a new contact
+    await contacts.createButton.click();
+
+    //step 3: make sure the user is navigated to the contact creation page
+    await expect(page).toHaveURL(/.*\/tasks\/create/);
+    const createContact = new CreateContact(page);
+    await expect(createContact.createContactHeader).toBeVisible();
+
+    // step 4: fill out the contact creation form with invalid data, always excluding one field
+    for (const field of Object.values(ContactData)) {
+      await createContact.fillFormExceptField(field);
+      await createContact.saveContact.click();
+      // step 5: make sure the error message is visible
+      await expect(createContact.errorMessage).toBeVisible();
+      // step 6: clear form for the next iteration
+      await createContact.clearForm();
+    }
   }
 );
