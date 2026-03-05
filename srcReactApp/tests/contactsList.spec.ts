08e1ac21f5eeeafbefcb3d065eb6151b56ed23da
Noa Milman <noamilman31@gmail.com>
2026-03-05 15:47:02 +0200

    Add testtype tag

--- srcReactApp/tests/contactsList.spec.ts
+++ srcReactApp/tests/contactsList.spec.ts
@@ -6,34 +6,36 @@ import reactAppTest from "./setup/testLevelHooks.setup";
 
 const contactsListTest = reactAppTest.extend({});
 
-/**
- * Search for a contact in the contacts list,
- * delete that contact, and validate that it reappears on refresh.
- */
-contactsListTest(
-  "Search for a contact and then delete it",
-  async ({ contacts, page }): Promise<void> => {
-    // The name of the contact to delete.
-    const name = "Alika Medina";
+contactsListTest.describe("Contacts List Tests", { tag: "@contacts" }, () => {
+  /**
+   * Search for a contact in the contacts list,
+   * delete that contact, and validate that it reappears on refresh.
+   */
+  contactsListTest(
+    "Search for a contact and then delete it",
+    async ({ contacts, page }): Promise<void> => {
+      // The name of the contact to delete.
+      const name = "Alika Medina";
 
-    // Accept any alerts that appear.
-    contacts().acceptAlerts();
+      // Accept any alerts that appear.
+      contacts().acceptAlerts();
 
-    // Search for the contact.
-    await contacts().search(name, 1);
+      // Search for the contact.
+      await contacts().search(name, 1);
 
-    // Delete the contact.
-    await contacts().deleteNthContact(0);
+      // Delete the contact.
+      await contacts().deleteNthContact(0);
 
-    // Validate that the contact has been deleted and the search shows no results.
-    await contacts().searchInput.fill("");
-    await contacts().search(name, 0);
+      // Validate that the contact has been deleted and the search shows no results.
+      await contacts().searchInput.fill("");
+      await contacts().search(name, 0);
 
-    // Refresh the page.
-    page.reload();
+      // Refresh the page.
+      page.reload();
 
-    // After the refresh, the page should be reinitialized.
-    // Search for the contact again and validate that it exists.
-    await contacts().search(name, 1);
-  }
-);
+      // After the refresh, the page should be reinitialized.
+      // Search for the contact again and validate that it exists.
+      await contacts().search(name, 1);
+    }
+  );
+});
